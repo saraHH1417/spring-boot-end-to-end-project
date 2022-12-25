0852557de3eec6f6033f206011f3937efc6b8279
saraHH1417 <sara.afzar@gmail.com>
2022-12-26 03:06:48 +0330

    feat: creating entity

---
+++ src/main/java/com/sara/studentdal/entities/Student.java
@@ -0,0 +1,66 @@
+package com.sara.studentdal.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.context.annotation.Primary;
+
+@Entity
+@Table(name = "studenttab")
+public class Student {
+
+    @Id
+    private Long id;
+
+    @Column(name = "sname")
+    private String name;
+
+    @Column(name = "scourse")
+    private String course;
+
+    @Column(name = "sfee")
+    private Double fee;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void setFee(Double fee) {
+        this.fee = fee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public Double getFee() {
+        return fee;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", course='" + course + '\'' +
+                ", fee=" + fee +
+                '}';
+    }
+}
