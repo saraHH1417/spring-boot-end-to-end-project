c64008a5ef21c02e507f34fda08fb2f9afc28618
saraHH1417 <sara.afzar@gmail.com>
2022-12-27 03:47:12 +0330

    chore: change directory structure 2

--- README.md
+++
@@ -1,1 +0,0 @@
-# spring-boot-end-to-end-project
--- src/main/java/com/sara/studentdal/repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sara.studentdal.repositories;
-
-import com.sara.studentdal.entities.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-
-}
